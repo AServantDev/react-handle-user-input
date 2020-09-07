549450ad8856650e0574855df5eba3dcf6bc5b5c
Arnaud Servant <34283027+AServantDev@users.noreply.github.com>
2020-09-07 17:51:51 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID L4UXI2WcZkG-Q9ikXlASbiSvnMvxadRkeW53Cq6HRgU",
+    Authorization: "Client-ID YOUR KEY",
   },
 });
